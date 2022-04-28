896b1ff1c23cf8ffd2c0ab0f73a052c5e7f1fbcb
Paweł Knioła <pawel.kn@gmail.com>
2022-04-28 15:10:37 +0200

    Example: Fix return values

--- example/example_rainflow.cpp
+++ example/example_rainflow.cpp
@@ -7,13 +7,13 @@ int main( int argc, char *argv[] )
 {
     if( argc < 2 ) {
         std::cout << "Usage: ./example [FILE]" << std::endl;
-        return false;
+        return 1;
     }
 
     std::ifstream is { argv[1] };
     if( ! is ) {
         std::cerr << "Cannot open the file: " << argv[1] << std::endl;
-        return false;
+        return 1;
     }
 
     std::istream_iterator<double> start { is }, end;    
@@ -25,4 +25,6 @@ int main( int argc, char *argv[] )
     std::cout << "counted cycles (binsize=" << binsize << "):" << std::endl;
     for( auto count : counts )
         std::cout << '(' << count.range << ", " << count.cycles << ')' << std::endl;
+
+    return 0;
 }

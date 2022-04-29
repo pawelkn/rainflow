a5b9de8954df53d223eb139879252c439bbfd8bb
Paweł Knioła <pawel.kn@gmail.com>
2022-04-29 14:16:33 +0200

    Example: Minor fix

--- example/example_rainflow.cpp
+++ example/example_rainflow.cpp
@@ -6,7 +6,7 @@
 int main( int argc, char *argv[] )
 {
     if( argc < 2 ) {
-        std::cout << "Usage: ./example [FILE]" << std::endl;
+        std::cout << "Usage: example_rainflow [FILE]" << std::endl;
         return 1;
     }
 
